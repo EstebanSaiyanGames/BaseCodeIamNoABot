c4f4b01c58ae2cdd36fc080c407405a1924240ba
⚡LaTostadoraSaiyan⚡ <adrian25.alv@gmail.com>
2022-02-05 15:00:37 -0800

    confirmar en "main"

---
+++ commands/test/infobot.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const { Client, MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "infobot",
+  alias: [],
+
+execute (client, message, args){
+
+    message.reply("Hola, Gracias Por usar el codigo base de ⚡I'mNotABot⚡, Mas informacion visite: https://iamnotabot.estebansaiyan.repl.co/")
+ }
+
+}
